4f7daeefe3be67a72e605c8c24e450615aa9cf6c
igor11212 <igorosypenko7@gmail.com>
2026-08-18 09:39:47 +0300

    Версія 20260818b у файлах рівнів

--- assets/data/a1.js
+++ assets/data/a1.js
@@ -1,5 +1,5 @@
-import { modules as part1 } from './a1-part1.js?v=20260818a';
-import { modules as part2 } from './a1-part2.js?v=20260818a';
+import { modules as part1 } from './a1-part1.js?v=20260818b';
+import { modules as part2 } from './a1-part2.js?v=20260818b';
 
 export default {
   intro:
--- assets/data/c1.js
+++ assets/data/c1.js
@@ -1,5 +1,5 @@
-import { modules as part2 } from './c1-part2.js?v=20260818a';
-import { modules as part3 } from './c1-part3.js?v=20260818a';
+import { modules as part2 } from './c1-part2.js?v=20260818b';
+import { modules as part3 } from './c1-part3.js?v=20260818b';
 
 export default {
   intro:
